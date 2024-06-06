2cd0bee43a86f74405ab6accb6792422f4de95ff
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-06-06 23:14:24 +0200

    Add implementation of the action
    This commit adds the core logic of the action that parses the XML and publish it into the step summary in GitHub.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,64 @@
+const fs = require('fs')
+const xml2js = require('xml2js')
 const core = require('@actions/core')
 
 async function run() {
+  // Read inputs of the action
   const filePath = core.getInput('file-path', { required: true })
   const displayOnlySurvived =
     core.getInput('display-only-survived', { required: false }) === 'true'
 
-  core.info(
-    'reading ${filePath} with the displayOnlySurvived = ${displayOnlySurvived}'
-  )
+  const parser = new xml2js.Parser()
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      core.setFailed(`Error reading XML file: ${err.message}`)
+      return
+    }
+
+    parser.parseString(data, (parseError, result) => {
+      if (parseError) {
+        core.setFailed(`Error parsing XML: ${parseError.message}`)
+        return
+      }
+
+      const mutationsResult = result.mutations.mutation
+      const fileGroups = mutationsResult.reduce((acc, mutation) => {
+        const file = mutation.sourceFile[0]
+        if (!acc[file]) acc[file] = []
+        const method = mutation.mutatedMethod[0]
+        const lineNumber = mutation.lineNumber[0]
+        const detected = mutation.$.detected === 'true' ? '✅' : '❌'
+        const status = mutation.$.status === 'KILLED' ? '💀' : '🚶'
+        const description = mutation.description[0]
+
+        if (!displayOnlySurvived || status === '🚶') {
+          acc[file].push(
+            `${detected} \`${method}\` (Line ${lineNumber}) - ${description} ${status}`
+          )
+        }
+        return acc
+      }, {})
+
+      let reportContent = `# Mutation Test Summary
+        
+## Overview
+
+This report provides an overview of mutation testing results, grouped by file. Each entry details a mutation attempt, its detection status, and specific mutation description.
+
+`
+
+      for (const [file, mutations] of Object.entries(fileGroups)) {
+        reportContent += `### Mutations in ${file}\n`
+        for (const mutation of mutations) {
+          reportContent += `- ${mutation}\n`
+        }
+        reportContent += '\n'
+      }
+
+      // Write directly to the GitHub Step Summary using @actions/core
+      core.summary.addRaw(reportContent).write()
+    })
+  })
 }
 
 module.exports = {
