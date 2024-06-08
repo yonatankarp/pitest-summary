914640cc2e5d39c3c0b3c1218c86d99a5c47d631
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-06-08 14:37:36 +0300

    Add test cases

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -6,7 +6,20 @@ const github = require('@actions/github')
 const main = require('../src/main')
 
 describe('action', () => {
-  it('dummy test', async () => {
-    expect(true).toBe(true)
-  })
+  test('should set failed if file-path input is missing', async () => {})
+
+  test('should handle file read error', async () => {})
+
+  test('should handle XML parse error', async () => {})
+
+  test('should handle unknown fields in XML', async () => {})
+
+  test('should generate report correctly with mutations', async () => {})
+
+  test('should handle displayOnlySurvived flag correctly', async () => {})
+
+  // This test focuses on the structure of the output reports, ensuring the right emojis have been selected etc
+  test('should generate correct report content', async () => {})
+
+  test('should handle empty XML correctly', async () => {})
 })
