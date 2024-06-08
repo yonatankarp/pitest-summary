d93aba450c1a9d4f3bfa862cb17deac9ee763f18
y0n1 <y0n1@outlook.com>
2024-06-09 00:32:50 +0300

    Refactor main.js in for async-style calls

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-const fs = require('fs')
+const fs = require('node:fs/promises')
 const xml2js = require('xml2js')
 const core = require('@actions/core')
+
+let reportContent = `
+# Mutation Test Summary
+        
+## Overview
+
+This report provides an overview of mutation testing results, grouped by file. Each entry details a mutation attempt, its detection status, and specific mutation description.
+
+`.trimStart() // Removes the first new line, before "# Mutation Test Summary"
 
 async function run() {
   // Read inputs of the action
@@ -9,18 +18,12 @@ async function run() {
     core.getInput('display-only-survived', { required: false }) === 'true'
 
   const parser = new xml2js.Parser()
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      core.setFailed(`Error reading XML file: ${err.message}`)
-      return
-    }
-
-    parser.parseString(data, (parseError, result) => {
-      if (parseError) {
-        core.setFailed(`Error parsing XML: ${parseError.message}`)
-        return
-      }
-
+  let data = null
+  try {
+    data = await fs.readFile(filePath)
+    let result = null
+    try {
+      result = await parser.parseStringPromise(data)
       const mutationsResult = result.mutations.mutation
       const fileGroups = mutationsResult.reduce((acc, mutation) => {
         const file = mutation.sourceFile[0]
@@ -39,14 +42,6 @@ async function run() {
         return acc
       }, {})
 
-      let reportContent = `# Mutation Test Summary
-        
-## Overview
-
-This report provides an overview of mutation testing results, grouped by file. Each entry details a mutation attempt, its detection status, and specific mutation description.
-
-`
-
       for (const [file, mutations] of Object.entries(fileGroups)) {
         reportContent += `### Mutations in ${file}\n`
         for (const mutation of mutations) {
@@ -57,8 +52,12 @@ This report provides an overview of mutation testing results, grouped by file. E
 
       // Write directly to the GitHub Step Summary using @actions/core
       core.summary.addRaw(reportContent).write()
-    })
-  })
+    } catch (parseError) {
+      core.setFailed(`Error parsing XML: ${parseError.message}`)
+    }
+  } catch (readError) {
+    core.setFailed(`Error reading XML file: ${readError.message}`)
+  }
 }
 
 module.exports = {
