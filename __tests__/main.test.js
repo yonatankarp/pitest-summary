21da106b4c182306a9bbc15d10a5d4e2495e51de
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-06-09 11:02:45 +0300

    Add action implementation (#6)
    This commit adds the core logic of the action that parses the XML and publishes it into the step summary in GitHub.
    
    Moreover, the PR adds an example test case for the action (more will follow in another commit).
    
    ---------
    
    Signed-off-by: yonatankarp <yonvata@gmail.com>
    Co-authored-by: y0n1 <y0n1@outlook.com>

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,12 +1,44 @@
 /**
  * Unit tests for the action's main functionality, src/main.js
  */
-const core = require('@actions/core')
-const github = require('@actions/github')
-const main = require('../src/main')
 
-describe('action', () => {
-  it('dummy test', async () => {
-    expect(true).toBe(true)
+const fs = require('node:fs/promises')
+// const xml2js = require('xml2js')
+const core = require('@actions/core')
+
+jest.mock('node:fs/promises')
+// jest.mock('xml2js')
+jest.mock('@actions/core')
+
+describe('/src/main.js', () => {
+  test('should set failed if file-path input is missing', async () => {
+    // arrange
+    const mockReadErrorMessage = 'File not found'
+    core.getInput.mockReturnValueOnce('stam_file.xml')
+    core.getInput.mockReturnValueOnce('false')
+    fs.readFile.mockRejectedValue(new Error(mockReadErrorMessage))
+
+    // act
+    await require('../src/main').run()
+
+    // assert
+    await expect(core.setFailed).toHaveBeenCalledWith(
+      `Error reading XML file: ${mockReadErrorMessage}`
+    )
   })
+
+  test('should handle file read error', async () => {})
+
+  test('should handle XML parse error', async () => {})
+
+  test('should handle unknown fields in XML', async () => {})
+
+  test('should generate report correctly with mutations', async () => {})
+
+  test('should handle displayOnlySurvived flag correctly', async () => {})
+
+  // This test focuses on the structure of the output reports, ensuring the right emojis have been selected etc
+  test('should generate correct report content', async () => {})
+
+  test('should handle empty XML correctly', async () => {})
 })
--- src/main.js
+++ src/main.js
@@ -1,13 +1,64 @@
+const fs = require('node:fs/promises')
+const xml2js = require('xml2js')
 const core = require('@actions/core')
 
-async function run() {
-  const filePath = core.getInput('file-path', { required: true })
-  const displayOnlySurvived =
-    core.getInput('display-only-survived', { required: false }) === 'true'
+let reportContent = `
+# Mutation Test Summary
+        
+## Overview
 
-  core.info(
-    'reading ${filePath} with the displayOnlySurvived = ${displayOnlySurvived}'
-  )
+This report provides an overview of mutation testing results, grouped by file. Each entry details a mutation attempt, its detection status, and specific mutation description.
+
+`.trimStart() // Removes the first new line, before "# Mutation Test Summary"
+
+async function run() {
+  // Read inputs of the action
+  const filePath = core.getInput('file-path', { required: true })
+  const displayOnlySurvived = core.getBooleanInput('display-only-survived', {
+    required: false
+  })
+
+  const parser = new xml2js.Parser()
+  let data = null
+  try {
+    data = await fs.readFile(filePath)
+    let result = null
+    try {
+      result = await parser.parseStringPromise(data)
+      const mutationsResult = result.mutations.mutation
+      const fileGroups = mutationsResult.reduce((acc, mutation) => {
+        const file = mutation.sourceFile[0]
+        if (!acc[file]) acc[file] = []
+        const method = mutation.mutatedMethod[0]
+        const lineNumber = mutation.lineNumber[0]
+        const detected = mutation.$.detected === 'true' ? '✅' : '❌'
+        const status = mutation.$.status === 'KILLED' ? '💀' : '🚶'
+        const description = mutation.description[0]
+
+        if (!displayOnlySurvived || status === '🚶') {
+          acc[file].push(
+            `${detected} \`${method}\` (Line ${lineNumber}) - ${description} ${status}`
+          )
+        }
+        return acc
+      }, {})
+
+      for (const [file, mutations] of Object.entries(fileGroups)) {
+        reportContent += `### Mutations in ${file}\n`
+        for (const mutation of mutations) {
+          reportContent += `- ${mutation}\n`
+        }
+        reportContent += '\n'
+      }
+
+      // Write directly to the GitHub Step Summary using @actions/core
+      await core.summary.addRaw(reportContent).write()
+    } catch (parseError) {
+      core.setFailed(`Error parsing XML: ${parseError.message}`)
+    }
+  } catch (readError) {
+    core.setFailed(`Error reading XML file: ${readError.message}`)
+  }
 }
 
 module.exports = {
